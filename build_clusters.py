cd38e619ad7e357bc85cb34fc01beb6e06532581
vicioms <vincenzo.schimmenti@gmail.com>
2023-10-17 15:14:36 +0200

    Process + Clustering

--- build_clusters.py
+++ build_clusters.py
@@ -7,18 +7,21 @@ parser = argparse.ArgumentParser(
                     prog='Thin film Data Analyzer - Clusters')
 
 parser.add_argument('--field', type=float, default=0.13) 
-parser.add_argument('--ds', type=int, default=3)
 parser.add_argument('--dt', type=int, default=10)
 parser.add_argument('--basename', default="raw_data/file_field")
 
 args = parser.parse_args()
 filename = args.basename + "%1.2f_events.pkl" % args.field
-with open(args.basename % args.field, 'rb') as file:
+with open(filename, 'rb') as file:
     xs, ys = pkl.load(file)
 frames = list(xs.keys())
 
+dt = args.dt
 
-print(frames)
-for f1 in frames:
-    for f2 in frames:
-        
+for counter, f1 in enumerate(frames):
+    x1, y1 = xs[f1], ys[f1]
+    for f2 in frames[counter+1:]:
+        if(f2-f1 > dt):
+            break
+        x2, y2 = xs[f2], ys[f2]
+        d_21 = np.min(np.abs(x1[None, :]-x2[:,None])+np.abs(y1[None, :]-y2[:,None]))
