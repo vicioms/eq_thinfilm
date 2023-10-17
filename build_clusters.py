dfa52f2bc0c5f820ed48655efa3aeb6c227bf085
vicioms <vincenzo.schimmenti@gmail.com>
2023-10-17 18:05:42 +0200

    Cluster extraction completed

--- build_clusters.py
+++ build_clusters.py
@@ -1,7 +1,7 @@
 import numpy as np
-import networkx as nx
 import pickle as pkl
 import argparse
+import scipy.sparse as sp
 
 parser = argparse.ArgumentParser(
                     prog='Thin film Data Analyzer - Clusters')
@@ -14,14 +14,20 @@ args = parser.parse_args()
 filename = args.basename + "%1.2f_events.pkl" % args.field
 with open(filename, 'rb') as file:
     xs, ys = pkl.load(file)
-frames = list(xs.keys())
+frames = np.sort(list(xs.keys()))
+max_frames = np.max(frames)+1
 
 dt = args.dt
 
+sparse_net = sp.dok_array(arg1=(max_frames, max_frames), dtype=int)
 for counter, f1 in enumerate(frames):
     x1, y1 = xs[f1], ys[f1]
     for f2 in frames[counter+1:]:
+        assert f2 > f1
         if(f2-f1 > dt):
             break
         x2, y2 = xs[f2], ys[f2]
         d_21 = np.min(np.abs(x1[None, :]-x2[:,None])+np.abs(y1[None, :]-y2[:,None]))
+        sparse_net[f1, f2] = d_21
+        sparse_net[f2, f1] = d_21
+sp.save_npz(args.basename + "%1.2f_net_dt=%i" % (args.field, dt), sparse_net.tocsr())
---
+++ cluster_statistics.py
@@ -0,0 +1,51 @@
+import numpy as np
+import networkx as nx
+import pickle as pkl
+import argparse
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(
+                    prog='Thin film Data Analyzer - Clusters')
+
+parser.add_argument('--field', type=float, default=0.13) 
+parser.add_argument('--dt', type=int, default=10)
+parser.add_argument('--ds', type=int, default=2)
+parser.add_argument('--basename', default="raw_data/file_field")
+
+args = parser.parse_args()
+sparse_net_filename = args.basename + "%1.2f_net_dt=%i.npz" % (args.field, args.dt)
+filename = args.basename + "%1.2f_events.pkl" % args.field
+with open(filename, 'rb') as file:
+    xs, ys = pkl.load(file)
+
+sparse_net  = sp.load_npz(sparse_net_filename)
+sparse_net[sparse_net > args.ds] = 0
+net = nx.Graph(sparse_net)
+clusters = []
+comps = nx.connected_components(net)
+for comp in comps:
+    clusters.append(np.sort(list(comp)))
+
+
+for cluster in clusters:
+    if(len(cluster) == 1 and cluster[0] not in xs):
+        continue
+    x_c = []
+    y_c = []
+    for c in cluster:
+        if(c in xs):
+            x_c.append(xs[c])
+            y_c.append(ys[c])
+    x_c = np.concatenate(x_c)
+    y_c = np.concatenate(y_c)
+    
+
+#cl_sizes = np.array([len(c) for c in clusters])
+#hist, edges = np.histogram(cl_sizes, bins=np.logspace(0.5,np.log10(np.max(cl_sizes)),20),density=True )
+#plt.scatter(edges[:-1],hist)
+#plt.plot(edges, edges**(-1.26))
+#plt.plot(edges, edges**(-1.11))
+#plt.xscale('log')
+#plt.yscale('log')
+#plt.show()
