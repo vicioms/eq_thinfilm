90e9d0038186275fdeb2a1af4c519efbc1bb9a22
vicioms <vincenzo.schimmenti@gmail.com>
2023-10-18 16:41:05 +0200

    Interface reconstruction + movie

--- intf_movie.py
+++ intf_movie.py
@@ -13,8 +13,39 @@ parser.add_argument('--basename', default="data/file_field")
 
 args = parser.parse_args()
 
-intf_filename = args.basename +  ("%1.2f" % args.field) + "_intf.npz"
+intf_filename = args.basename +  ("%1.2f" % args.field) + "_intf.npy"
 
 
-interface = np.load(intf_filename)[0]
-print(interface)
+interface = np.load(intf_filename)
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+ax.set_ylim(-25,25)
+x = np.linspace(-np.pi, np.pi, interface.shape[1])
+line, = ax.plot(x, interface[0]-interface[0].mean(), color='black')
+
+
+def animate(i):
+    line.set_ydata(interface[i,:]-interface[i,:].mean())  # update the data.
+    return line,
+
+
+ani = animation.FuncAnimation(
+    fig, animate, interval=2, blit=True, save_count=50)
+
+# To save the animation, use e.g.
+#
+# ani.save("movie.mp4")
+#
+# or
+#
+# writer = animation.FFMpegWriter(
+#     fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# ani.save("movie.mp4", writer=writer)
+
+plt.show()
